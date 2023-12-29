2ff173a1faae3747db66490ce9b823c80625bc48
aayush105 <sthaaayush105@gmail.com>
2023-12-30 00:20:29 +0545

    feat: work on about section and social routing

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,21 @@
+// next links
+import Link from "next/link";
+
+// icons
+import { RiTwitterLine, RiLinkedinLine, RiGithubLine, RiMailLine} from "react-icons/ri"
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className="flex items-center gap-x-5 text-lg">
+    <Link href={'https://www.linkedin.com/in/aayushrestha/'} className="hover:text-accent transition-all duration-300">
+      <RiLinkedinLine />
+    </Link>
+    <Link href={'https://github.com/aayush105'} className="hover:text-accent transition-all duration-300">
+      <RiGithubLine />
+    </Link>
+    <Link href={'https://twitter.com/aayushrestha5'} className="hover:text-accent transition-all duration-300">
+      <RiTwitterLine />
+    </Link>
+  </div>;
 };
 
 export default Socials;
