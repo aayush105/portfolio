1e27f5649eac396e08c8396fbad4a99f41b4a324
aayush105 <sthaaayush105@gmail.com>
2023-12-30 00:20:29 +0545

    feat: work on about section and social routing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "621ee7ccbbdfa6d5"; }
+/******/ 		__webpack_require__.h = function() { return "a3c7caa96d0ceea8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
